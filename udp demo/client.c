06fed7019a56d4f04dce5c90efd0807b486e0201
FNicon <mybeddrom@gmail.com>
2017-10-13 23:11:46 -0700

    renaming variables in client.c and server.c

--- udp demo/client.c
+++ udp demo/client.c
@@ -1,6 +1,6 @@
 /*
-    Simple udp client
-    Taken from: http://www.binarytides.com/programming-udp-sockets-c-linux/
+Simple udp client
+Taken from: http://www.binarytides.com/programming-udp-sockets-c-linux/
 */
 #include<stdio.h> //printf
 #include<string.h> //memset
@@ -9,60 +9,46 @@
 #include<sys/socket.h>
  
 #define SERVER "127.0.0.1"
-#define BUFLEN 512  //Max length of buffer
-#define PORT 8888   //The port on which to send data
- 
-void die(char *s)
-{
-    perror(s);
-    exit(1);
+#define BUFFER_LENGTH 512//Max length of buffer
+#define PORT 8888 //The port on which to send data
+
+void die(char *errorMessage) {
+	perror(errorMessage);
+	exit(1);
 }
- 
-int main(void)
-{
-    struct sockaddr_in si_other;
-    int s, i, slen=sizeof(si_other);
-    char buf[BUFLEN];
-    char message[BUFLEN];
- 
-    if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-    {
-        die("socket");
-    }
- 
-    memset((char *) &si_other, 0, sizeof(si_other));
-    si_other.sin_family = AF_INET;
-    si_other.sin_port = htons(PORT);
-     
-    if (inet_aton(SERVER , &si_other.sin_addr) == 0) 
-    {
-        fprintf(stderr, "inet_aton() failed\n");
-        exit(1);
-    }
- 
-    while(1)
-    {
-        printf("Enter message : ");
-        gets(message);
-         
-        //send the message
-        if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1)
-        {
-            die("sendto()");
-        }
-         
-        //receive a reply and print it
-        //clear the buffer by filling null, it might have previously received data
-        memset(buf,'\0', BUFLEN);
-        //try to receive some data, this is a blocking call
-        if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1)
-        {
-            die("recvfrom()");
-        }
-         
-        puts(buf);
-    }
- 
-    close(s);
-    return 0;
+int main(void) {
+	struct sockaddr_in clientAddress;
+	int socketConnector;
+	int socketSize=sizeof(clientAddress);
+	char buffer[BUFFER_LENGTH];
+	char message[BUFFER_LENGTH];
+	socketConnector = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (socketConnector == -1) {
+		die("socket");
+	}
+	memset((char *) &clientAddress, 0, sizeof(clientAddress));
+	clientAddress.sin_family = AF_INET;
+	clientAddress.sin_port = htons(PORT);
+	if (inet_aton(SERVER , &clientAddress.sin_addr) == 0) {
+		fprintf(stderr, "inet_aton() failed\n");
+		exit(1);
+	}
+	while(1) {
+		printf("Enter message : ");
+		gets(message);
+		//send the message
+		if (sendto(socketConnector, message, strlen(message) , 0 , (struct sockaddr *) &clientAddress, socketSize)==-1) {
+			die("sendto()");
+		}
+		//receive a reply and print it
+		//clear the buffer by filling null, it might have previously received data
+		memset(buffer,'\0', BUFFER_LENGTH);
+		//try to receive some data, this is a blocking call
+		if (recvfrom(socketConnector, buffer, BUFFER_LENGTH, 0, (struct sockaddr *) &clientAddress, &socketSize) == -1) {
+			die("recvfrom()");
+		}
+		puts(buffer);
+	}
+	close(socketConnector);
+	return 0;
 }
--- udp demo/server.c
+++ udp demo/server.c
@@ -1,70 +1,58 @@
 /*
-    Simple udp server
+Simple udp server
 */
 #include<stdio.h> //printf
 #include<string.h> //memset
 #include<stdlib.h> //exit(0);
 #include<arpa/inet.h>
 #include<sys/socket.h>
- 
-#define BUFLEN 512  //Max length of buffer
-#define PORT 8888   //The port on which to listen for incoming data
- 
-void die(char *s)
-{
-    perror(s);
-    exit(1);
+
+#define BUFFER_LENGTH 512//Max length of buffer
+#define PORT 8888 //The port on which to listen for incoming data
+
+void die(char *errorMessage) {
+	perror(errorMessage);
+	exit(1);
 }
- 
-int main(void)
-{
-    struct sockaddr_in si_me, si_other;
-     
-    int s, i, slen = sizeof(si_other) , recv_len;
-    char buf[BUFLEN];
-     
-    //create a UDP socket
-    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-    {
-        die("socket");
-    }
-     
-    // zero out the structure
-    memset((char *) &si_me, 0, sizeof(si_me));
-     
-    si_me.sin_family = AF_INET;
-    si_me.sin_port = htons(PORT);
-    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-     
-    //bind socket to port
-    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
-    {
-        die("bind");
-    }
-     
-    //keep listening for data
-    while(1)
-    {
-        printf("Waiting for data...");
-        fflush(stdout);
-         
-        //try to receive some data, this is a blocking call
-        if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
-        {
-            die("recvfrom()");
-        }
-         
-        //print details of the client/peer and the data received
-        printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-        printf("Data: %s\n" , buf);
-         
-        //now reply the client with the same data
-        if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)
-        {
-            die("sendto()");
-        }
-    }
- 
-    close(s);
-    return 0;
+int main(void) {
+	struct sockaddr_in serverAddress;
+	struct sockaddr_in clientAddress;
+	int socketConnector;
+	int socketSize = sizeof(clientAddress);
+	int receivedLength;
+	char buffer[BUFFER_LENGTH];
+	//create a UDP socket
+	socketConnector = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (socketConnector == -1) {
+		die("socket");
+	}
+	// zero out the structure
+	memset((char *) &serverAddress, 0, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(PORT);
+	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	//bind socket to port
+	if(bind(socketConnector, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+		die("bind");
+	}
+	//keep listening for data
+	while(1) {
+		memset(buffer,'\0', BUFFER_LENGTH);
+		printf("Waiting for data...");
+		fflush(stdout);
+		//try to receive some data, this is a blocking call
+		receivedLength = recvfrom(socketConnector, buffer, BUFFER_LENGTH, 0, (struct sockaddr *)&clientAddress, &socketSize);
+		if (receivedLength == -1) {
+			die("recvfrom()");
+		}
+		//print details of the client/peer and the data received
+		printf("Received packet from %s:%d\n", inet_ntoa(clientAddress.sin_addr), ntohs(clientAddress.sin_port));
+		printf("Data: %s\n" , buffer);
+		//now reply the client with the same data
+		if (sendto(socketConnector, buffer, receivedLength, 0, (struct sockaddr*) &clientAddress, socketSize) == -1) {
+			die("sendto()");
+		}
+	}
+	close(socketConnector);
+	return 0;
 }
